aa9564afc2526809ec253733a5910d5f47a1afb5
PrajwolKhadka <prazolkhadka67@gmail.com>
2025-12-10 13:29:17 +0545

    build

--- my-portfolio/src/components/chatbot.tsx
+++ my-portfolio/src/components/chatbot.tsx
@@ -28,7 +28,7 @@ const Chatbot: React.FC = () => {
     setBotTyping(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/chat", {
+      const res = await fetch("https://personalportfolio-lhwo.onrender.com/api/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: userMessage }),
